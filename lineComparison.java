2fb402edbea2c65d85fcd36e71f9667f50b57cba
saitarun135 <saitarun135@gm.com>
2021-11-02 17:44:45 +0530

    START:- Displayed welcome message on master branch.

---
+++ lineComparison.java
@@ -0,0 +1,6 @@
+/* START:- Display welcome mssg on master branch. */
+public class lineComparison{
+	public static void main(String[] args){
+		System.out.println("Welcome To Line Comparison Computation Program.\n");
+	}
+}
